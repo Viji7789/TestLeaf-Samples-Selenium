1608859a3831fdbe939c35be712683b9f6e4a87a
viji7 <viji7@192.168.0.70>
2021-11-11 15:23:08 -0500

    initial commit

---
+++ JavaPrograms/src/com/viji/javapgms/ArithmeticOperationsSample.java
@@ -0,0 +1,24 @@
+package com.viji.javapgms;
+
+import java.util.Scanner;
+
+public class ArithmeticOperationsSample {
+
+	
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter first value:");
+		int a=sc.nextInt();
+		
+		System.out.println("Enter second value:");
+		int b=sc.nextInt();
+		
+		int c;
+		c=a+b;
+		System.out.println("Total added value is:"+c);
+		
+	}
+
+}
