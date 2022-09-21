8624e377cf4d9a424540bf9d01115280e83c1bb5
Viji7789 <94017112+Viji7789@users.noreply.github.com>
2022-09-21 18:36:44 -0400

    Update ArithmeticOperationsSample.java
    added subtraction

--- JavaPrograms/src/com/viji/javapgms/ArithmeticOperationsSample.java
+++ JavaPrograms/src/com/viji/javapgms/ArithmeticOperationsSample.java
@@ -17,7 +17,17 @@ public class ArithmeticOperationsSample {
 		
 		int c;
 		c=a+b;
-		System.out.println("Total added value is:"+c);
+		System.out.println("The sum is:"+c);
+		
+		System.out.println("Enter first value:");
+		int i=sc.nextInt();
+		
+		System.out.println("Enter second value:");
+		int j=sc.nextInt();
+		
+		int k;
+		k=i-j;
+		System.out.println("The difference is:"+k);
 		
 	}
 
